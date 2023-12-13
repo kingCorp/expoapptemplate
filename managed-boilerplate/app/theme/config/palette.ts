39ea16bb33e8a68e197c0d36d4bc167f15f62ba9
Attasiem James Kingsley <attasiemj@gmail.com>
2023-12-13 04:28:59 +0100

    Initial commit

---
+++ managed-boilerplate/app/theme/config/palette.ts
@@ -0,0 +1,9 @@
+export const palette = {
+  black: "#000000",
+  black40: "rgba(0, 0, 0, 0.4)",
+  error: "#FF0101",
+  success: "#76EE59",
+  transparent: "transparent",
+  white: "#FFFFFF",
+  white40: "rgba(255, 255, 255, 0.4)",
+};
